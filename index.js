983520633959545b1c7abde6b255578f566b975d
Vitor Lamartine <vitorlamartine21@gmail.com>
2021-03-10 22:18:37 -0300

    Added option to use previous pulses

--- index.js
+++ index.js
@@ -1,76 +1,105 @@
 const urls = {
-  nist: 'https://beacon.nist.gov/beacon/2.0/pulse/time/previous/',
-  inmetro: 'https://beacon.inmetro.gov.br/beacon/2.0/pulse/time/previous/',
-  uchile: 'https://random.uchile.cl/beacon/2.0/pulse/time/previous/',
+  nist: 'https://beacon.nist.gov/beacon/2.0/pulse/time/',
+  inmetro: 'https://beacon.inmetro.gov.br/beacon/2.0/pulse/time/',
+  uchile: 'https://random.uchile.cl/beacon/2.0/pulse/time/',
 }
+
+
 const shuffle = async () => {
-  const seedOrigin = document.getElementById("seedOrigin").value;
+  const { seedOrigin, items } = getFormData();
   let seed;
   if(seedOrigin === 'user'){
     seed = document.getElementById('seed').value || Date.now();
-    document.getElementById('msg').classList.add('display-none');
-    document.getElementById('pulse').classList.add('display-none');
+    addClass('msg', 'display-none');
+    addClass('pulse', 'display-none');
   } else {
-    const timestampShuffle = Date.now();
-    const dateShuffle = new Date(timestampShuffle);
 
-    let remainingTime = waitTime = 60 - dateShuffle.getSeconds()
-    const timestampPulse = timestampShuffle + remainingTime * 1000;
-    const datePulse = new Date(timestampPulse);
+    const shuffle = {
+      timestamp: Date.now(),
+      date: new Date(Date.now()),
+      element: document.getElementById("dateShuffle")
+    }
+    let waitTime = 60 - shuffle.date.getSeconds();
+    const pulse = getPulseTimestamp(shuffle.timestamp, waitTime);
 
-    // const url = urls[seedOrigin] + timestampShuffle;
-    const url = urls[seedOrigin] + timestampPulse;
-    const msgElemnt = document.getElementById('msg'); 
-    msgElemnt.classList.remove('display-none');
+    const { value } = document.querySelector(`input[type='radio'][name='time']:checked`);
+    let url = urls[seedOrigin];
+    if(value === "future"){
+      url += 'previous/';
+      removeClass('msg', 'display-none');
 
-    document.getElementById("dateShuffle").textContent =`${dateShuffle.getHours()}:${dateShuffle.getMinutes() < 10 ? "0" + dateShuffle.getMinutes().toString() : dateShuffle.getMinutes()}`
-    document.getElementById("datePulse").textContent = `${datePulse.getHours()}:${datePulse.getMinutes() < 10 ? "0" + datePulse.getMinutes().toString() : datePulse.getMinutes()}`;
-    const remainingTimeElement = document.getElementById("remainingTime");
-    remainingTimeElement.textContent = remainingTime
+      shuffle.element.textContent =`${shuffle.date.getHours()}:${shuffle.date.getMinutes() < 10 ? "0" + shuffle.date.getMinutes().toString() : shuffle.date.getMinutes()}`
+      pulse.element.textContent = `${pulse.date.getHours()}:${pulse.date.getMinutes() < 10 ? "0" + pulse.date.getMinutes().toString() : pulse.date.getMinutes()}`;
 
-    const interval = setInterval(() => {
-      remainingTimeElement.textContent = remainingTime
-      remainingTime--;
+      const remainingTimeElement = document.getElementById("remainingTime");
       
-    }, 1000);
+      remainingTimeElement.textContent = waitTime
+      
+      const interval = setInterval(() => {
+        remainingTimeElement.textContent = waitTime
+        waitTime--;
+      }, 1000);
+    
+      await wait((waitTime+1) * 1000);
+      addClass('msg', 'display-none');
+      
+      clearInterval(interval);
+    }
 
+    url += pulse.timestamp;
 
-    await wait((remainingTime+1) * 1000);
-    msgElemnt.classList.add('display-none');
-    clearInterval(interval);
+    const {outputValue, pulseIndex} = await fetchPulse(url);
 
-    const response = await fetch(url);
-    const pulse = (await response.json()).pulse;
-    seed = pulse.outputValue;
-    document.getElementById('pulse').classList.remove('display-none');
-    document.getElementById('pulseIndex').textContent = pulse.pulseIndex;
+    seed = outputValue;
+    removeClass('pulse', 'display-none');
+    document.getElementById('pulseIndex').textContent = pulseIndex;
   }
-  const items = document.getElementById('items').value.split('\n');
 
-  const hashedItems = await Promise.all(items.map(async (item, i) => {
+  const hashedItems = await hashItems(items.split("\n"));
+
+  hashedItems.sort((a, b) => a.hash.localeCompare(b.hash));
+
+  document.getElementById('baseSeed').textContent = seed;
+  displayList(hashedItems);
+}
+
+const getPulseTimestamp = (timestamp, waitTime) => {
+  const { value } = document.querySelector(`input[type='radio'][name='time']:checked`);
+
+  if(value === "future"){
+    return {
+      timestamp: timestamp + waitTime * 1000,
+      date: new Date(timestamp + waitTime * 1000),
+      element: document.getElementById("datePulse")
+    }
+  } else {
+    const [ year, month, day ] = document.getElementById('date').value.split('-');
+    const [ hour, minute ] = document.getElementById('time').value.split(':');
+    
+    const date = new Date(year, month - 1, day, hour, minute);
+    console.log(date);
+    return {
+      date,
+      timestamp: date.getTime(),
+      element: document.getElementById("datePulse")
+    }
+  }
+}
+
+const fetchPulse = async (url) => {
+  const json = await fetch(url);
+  const response = await json.json();
+
+  return response.pulse;
+}
+
+const hashItems = (items, seed) => {
+  return Promise.all(items.map(async (item, i) => {
     return {
       hash: await sha256(`${i+1}${item}${seed}`),
       value: item
     };
-  }));
-
-  hashedItems.sort((a, b) => a.hash.localeCompare(b.hash));
-
-  const list = document.getElementById('resultList');
-  const usedSeed = document.getElementById('baseSeed');
-
-  list.innerHTML = '';
-
-  hashedItems.forEach(item => {
-    const li = document.createElement("li");
-    const value = document.createTextNode(`(0x${item.hash.substr(0, 10).toUpperCase()}...) ${item.value}`);
-    li.appendChild(value);
-
-    list.appendChild(li);
-  });
-
-  usedSeed.textContent = seed;
-  list.classList.remove('display-none');
+  }))
 }
 
 const sha256 = async (message) => {
@@ -91,9 +120,74 @@ const sha256 = async (message) => {
 const handleSeedOriginChange = (element) => {
   if(element.value === 'user'){
     document.getElementById('seed_label').classList.remove('display-none');
+    document.getElementById('timeSeed').classList.add('display-none');
   } else {
     document.getElementById('seed_label').classList.add('display-none');
+    document.getElementById('timeSeed').classList.remove('display-none');
   }
+}
+
+const setMaxDate = () => {
+  const today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth() + 1;
+  let yyyy = today.getFullYear();
+
+  if(dd < 10) dd = '0'+dd;
+  if(mm < 10) mm = '0'+mm;
+  document.getElementById('date').setAttribute('max', `${yyyy}-${mm}-${dd}`);
+}
+
+const handleTimeChange = () => {
+
+  const time = document.getElementById('time').value;
+  const date = document.getElementById('date').value;
+
+  if(!time || !date) return;
+
+  const [hour, minutes] = time.split(':');
+  
+  const today = new Date();
+  
+  today.setHours(hour);
+  today.setMinutes(minutes);
+
+  let dd = today.getDate();
+  let mm = today.getMonth() + 1;
+  let yyyy = today.getFullYear();
+  if(dd < 10) dd = '0'+dd;
+  if(mm < 10) mm = '0'+mm;
+
+  if(date && date == `${yyyy}-${mm}-${dd}` && today.getTime() > Date.now()){
+    removeClass('timeError', 'display-none');
+    document.getElementById("submit").setAttribute('disabled', true);
+  } else {
+    document.getElementById("submit").removeAttribute('disabled');
+    addClass('timeError', 'display-none');
+  }
+}
+
+const handlePulseTimeChange = ({ value }) => {
+  if(value == "past"){
+    document.getElementById('datetime').classList.remove('display-none');
+  } else {
+    document.getElementById('datetime').classList.add('display-none');
+  }
+}
+
+const displayList = (items) => {
+  const list = document.getElementById('resultList');
+  list.innerHTML = '';
+
+  items.forEach(item => {
+    const li = document.createElement("li");
+    const value = document.createTextNode(`(0x${item.hash.substr(0, 10).toUpperCase()}...) ${item.value}`);
+    li.appendChild(value);
+
+    list.appendChild(li);
+  });
+
+  list.classList.remove('display-none');
 }
 
 const wait = (ms) => {
@@ -103,3 +197,21 @@ const wait = (ms) => {
 const changeLanguage = (language) => {
   this.location.pathname = this.location.pathname.replace(/\/[a-z]{2}\//, `/${language.value}/`);
 }
+
+const getFormData = () => {
+  const seedOrigin = document.getElementById('seedOrigin').value;
+  const items = document.getElementById('items').value;
+  return { seedOrigin, items };
+}
+
+const addClass = (elementId, className) => {
+  document.getElementById(elementId).classList.add(className);
+}
+
+const removeClass = (elementId, className) => {
+  document.getElementById(elementId).classList.remove(className);
+}
+
+window.onload = e => {
+  setMaxDate();
+};
